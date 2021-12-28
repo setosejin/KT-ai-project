40eac6b07898fb11723e78bcb55ea4f11ca22db9
Sejin Kim <lemon05090@naver.com>
2021-12-28 22:08:04 +0900

    make test page with react

---
+++ morning/src/App.js
@@ -0,0 +1,32 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="alarm">Active</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="clothes">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="mask">Link</a>
+          </li>
+          
+        </ul>
+      </header>
+
+      <body className="App-body">
+
+        여기에 카메라 화면이 들어갑니다.
+
+      </body>
+    </div>
+  );
+}
+
+export default App;
