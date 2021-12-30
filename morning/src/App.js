240c0d41b4c34a5f756bb4e62bed35331438fb13
Sejin Kim <lemon05090@naver.com>
2021-12-30 10:51:47 +0900

    add web camera module

--- morning/src/App.js
+++ morning/src/App.js
@@ -2,28 +2,43 @@
 import './App.css';
 
 function App() {
+
+  var video = document.querySelector("#videoElement");
+ 
+  if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(function (stream) {
+        video.srcObject = stream;
+      })
+      .catch(function (err0r) {
+        console.log("Something went wrong!");
+      });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         
         <ul class="nav">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="alarm">Active</a>
+            <a class="nav-link active" aria-current="page" href="alarm">Alarm</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="clothes">Link</a>
+            <a class="nav-link" href="clothes">Clothes</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="mask">Link</a>
+            <a class="nav-link" href="mask">Mask</a>
           </li>
           
         </ul>
       </header>
 
       <body className="App-body">
-
-        여기에 카메라 화면이 들어갑니다.
-
+        <div id="container">
+          <video autoplay="true" id="videoElement">
+      
+          </video>
+        </div>
       </body>
     </div>
   );
